d30be29d7d1dcb903bf28f06e937ab0c830236d5
rogerluo410 <roger@rogers-MacBook-Air.local>
2016-01-27 15:05:15 +0800

    add stylesheet.

--- app/controllers/reply_controller.rb
+++ app/controllers/reply_controller.rb
@@ -9,10 +9,13 @@ class ReplyController < ApplicationController
     @post = Post.find(params[:post_id])
     @post.replys.build({content: params[:reply][:content].presence, author_id: params[:reply][:author_id].presence})
     if @post.save
-      redirect_to action: 'index'
+      @reply = Reply.new
+      render 'index'
+      #redirect_to action: 'index'
     else
       #redirect_to '/post/#{@post.id}/reply', :alert => @post.errors.full_messages.join(',')
-      redirect_to action: 'index'
+      @reply = Reply.new
+      render 'index'
     end
   end
 
